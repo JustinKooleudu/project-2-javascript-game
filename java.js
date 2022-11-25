b59830dc76de08ac9c2cf2fad68258daa95d4787
JustinKooledu <9019785@student.zadkine.nl>
2022-11-25 09:56:46 +0100

    0

--- java.js
+++
@@ -1,6 +0,0 @@
-var teller = 0   //click teller
-
-function click(){
-    document.getElementById("teller").innerHTML = "lemons: " + teller++;
-    // ^ zegt dat de text moet veranderen naar "lemons: (nummer + 1)"
-}
